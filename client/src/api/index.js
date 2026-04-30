4cbbf1a299bf1886e4f9c8ae6818e76e3b3abf61
yassinezayane11 <yassinezayane11@gmail.com>
2026-04-30 23:31:46 +0100

    first commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || '/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
-const api = axios.create({ baseURL: API_URL });
+const api = axios.create({ baseURL: `${API_URL}/api` });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('dbach_admin_token');
